96eb743fd419da9a86131bb598d5765c1d297cac
EmilWong <emiloffice@gmail.com>
2017-09-13 11:29:32 +0800

    delete laravel content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'User Center | Multiverse Entertainment LLC') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
