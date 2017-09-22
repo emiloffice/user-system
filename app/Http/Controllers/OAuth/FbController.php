60ab7a8eba33e6f03488c4a9355b8ed7ea0fb82d
EmilWong <emiloffice@gmail.com>
2017-09-22 17:09:13 +0800

    modify login and reg

--- app/Http/Controllers/OAuth/FbController.php
+++ app/Http/Controllers/OAuth/FbController.php
@@ -2,51 +2,14 @@
 
 namespace App\Http\Controllers\OAuth;
 
-use Facebook\Exceptions\FacebookResponseException;
-use Facebook\Exceptions\FacebookSDKException;
-use Facebook\Facebook;
-use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Client;
+use App\UserAuth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Laravel\Socialite\Facades\Socialite;
+use League\OAuth1\Client\Server\User;
+use Ramsey\Uuid\Uuid;
 class FbController extends Controller
 {
-    /*public function login(Facebook $fb)
-    {
-        $helper = $fb->getCanvasHelper();
-
-        try {
-            $accessToken = $helper->getAccessToken();
-        } catch(FacebookResponseException $e) {
-            // When Graph returns an error
-            echo 'Graph returned an error: ' . $e->getMessage();
-            exit;
-        } catch(FacebookSDKException $e) {
-            // When validation fails or other local issues
-            echo 'Facebook SDK returned an error: ' . $e->getMessage();
-            exit;
-        }
-
-        if (! isset($accessToken)) {
-            echo 'No OAuth data could be obtained from the signed request. User has not authorized your app yet.';
-            exit;
-        }
-
-// Logged in
-        echo '<h3>Signed Request</h3>';
-        var_dump($helper->getSignedRequest());
-
-        echo '<h3>Access Token</h3>';
-        var_dump($accessToken->getValue());
-    }*/
-    public function login(Facebook $fb)
-    {
-        $helper = $fb->getRedirectLoginHelper();
-        $permissions = ['email']; // Optional permissions
-        $loginUrl = $helper->getLoginUrl('http://user.multiverseinc.com/fb-callback.php', $permissions);
-        echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
-    }
     public function redirectToProvider()
     {
         return Socialite::driver('facebook')->redirect();
@@ -54,79 +17,53 @@ class FbController extends Controller
     public function handleProviderCallback(Request $request)
     {
             $user = Socialite::driver('facebook')->user();
-            $client = new Client(); //GuzzleHttp\Client
-            /*$result = $client->get('https://graph.facebook.com/oauth/client_code?', [
-                'access_token' => $user->token,
-                'client_id' => env('FACEBOOK_APP_ID'),
-                'client_secret'=> env('FACEBOOK_APP_SECRET'),
-                'redirect_uri'=>'http://user.multiverseinc.com/OAuth/fb-callback'
-            ]);
-            $user->result = $result;*/
-            dd($user);
+            if ($user){
+                $res = User::where('email', $user->email)->first();
+                if ($res){
+                    $user_auth = new UserAuth;
+                    $user_auth->guid = $res->guid;
+                    $user_auth->identity_type = 7;//from facebook
+                    $user_auth->identity = 'facebook';//from facebook
+                    $user_auth->certificate = $user->token;//access_token
+                    $user_auth-save();
+                    $res->nick_name = $user->name;
+                    $user->gender=='male' ? $res->gender = 1 : $res->gender = 2;
+                    $res->face = $user->avatar;
+                    $res->face200 = $user->avatar;
+                    $res->faceSrc = $user->avatar_original;
+                    $res->update();
+                }else{
+                    $guid = Uuid::generate();
+                    $u = new User;
+                    $u->guid = $guid;
+                    $u->email = $user->email;
+                    $u->password = bcrypt('123456');
+                    $u->nick_name = $user->name;
+                    $u->gender=='male' ? $res->gender = 1 : $res->gender = 2;
+                    $u->face = $user->avatar;
+                    $u->face200 = $user->avatar;
+                    $u->faceSrc = $user->avatar_original;
+                    $u->save();
+                    if ($u){
+                        $user_auth = new UserAuth;
+                        $user_auth->guid = $res->guid;
+                        $user_auth->identity_type = 7;//from facebook
+                        $user_auth->identity = 'facebook';//from facebook
+                        $user_auth->certificate = $user->token;//access_token
+                        $user_auth-save();
+                        Auth::attempt(['email'=>$user->email, 'password'=>'123456']);
+                        return redirect('user-center');
+                    }
+
+
+                }
+            }else{
+                return redirect('login');
+            }
     }
 
     public function getAccessToken(Request $request)
     {
         dd($request);
-    }
-    public function callback(Facebook $fb)
-    {
-        $helper = $fb->getRedirectLoginHelper();
-        try {
-            $accessToken = $helper->getAccessToken();
-        } catch(FacebookResponseException $e) {
-            // When Graph returns an error
-            echo 'Graph returned an error: ' . $e->getMessage();
-            exit;
-        } catch(FacebookSDKException $e) {
-            // When validation fails or other local issues
-            echo 'Facebook SDK returned an error: ' . $e->getMessage();
-            exit;
-        }
-        if (! isset($accessToken)) {
-            if ($helper->getError()) {
-                header('HTTP/1.0 401 Unauthorized');
-                echo "Error: " . $helper->getError() . "\n";
-                echo "Error Code: " . $helper->getErrorCode() . "\n";
-                echo "Error Reason: " . $helper->getErrorReason() . "\n";
-                echo "Error Description: " . $helper->getErrorDescription() . "\n";
-            } else {
-                header('HTTP/1.0 400 Bad Request');
-                echo 'Bad request';
-            }
-            exit;
-        }
-        // Logged in
-        echo '<h3>Access Token</h3>';
-        var_dump($accessToken->getValue());
-
-        // The OAuth 2.0 client handler helps us manage access tokens
-        $oAuth2Client = $fb->getOAuth2Client();
-
-        // Get the access token metadata from /debug_token
-        $tokenMetadata = $oAuth2Client->debugToken($accessToken);
-        echo '<h3>Metadata</h3>';
-        var_dump($tokenMetadata);
-
-        // Validation (these will throw FacebookSDKException's when they fail)
-                $tokenMetadata->validateAppId(''); // Replace {app-id} with your app id
-        // If you know the user ID this access token belongs to, you can validate it here
-        //$tokenMetadata->validateUserId('123');
-        $tokenMetadata->validateExpiration('280507985774061');
-
-        if (! $accessToken->isLongLived()) {
-            // Exchanges a short-lived access token for a long-lived one
-            try {
-                $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
-            } catch (FacebookSDKException $e) {
-                echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
-                exit;
-            }
-
-            echo '<h3>Long-lived</h3>';
-            var_dump($accessToken->getValue());
-        }
-
-        $_SESSION['fb_access_token'] = (string) $accessToken;
     }
 }
