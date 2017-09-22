16cac9111e98ba88d594805ae7640e19515baa05
EmilWong <emiloffice@gmail.com>
2017-09-22 17:37:29 +0800

    modify facebook oauth

--- app/Http/Controllers/OAuth/FbController.php
+++ app/Http/Controllers/OAuth/FbController.php
@@ -8,6 +8,7 @@ use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Laravel\Socialite\Facades\Socialite;
 use Webpatser\Uuid\Uuid;
 
